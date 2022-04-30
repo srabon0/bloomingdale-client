fbcdbc93b8d675d1a7ca4fd0d0b634a0915c79c0
srabon0 <srabonemam4@gmail.com>
2022-04-30 16:41:07 +0600

    what a change

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -5,7 +5,7 @@ import { PencilAltIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
 import './header.css'; 
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import Loading from '../Loading/Loading';
 import { signOut } from 'firebase/auth';
 
@@ -87,63 +87,67 @@ export default function Header() {
                 </button>
 
                 {/* Profile dropdown */}
-                <Menu as="div" className="ml-3 relative">
-                  <div>
-                    <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="z-10 origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <NavLink
-                            to='/profile'
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            {user?.email} Profile
-                          </NavLink>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <NavLink
-                            to="/"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Settings
-                          </NavLink>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                          onClick={()=>signOut(auth)}
-                           className={classNames(
-                             active ? "bg-gray-100" : "",
-                             "block px-4 py-2 text-sm text-gray-700"
-                           )}
+                
+               {
+                 user ?  <Menu as="div" className="ml-3 relative">
+                 <div>
+                   <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                     <span className="sr-only">Open user menu</span>
+                     <img
+                       className="h-8 w-8 rounded-full"
+                       src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                       alt=""
+                     />
+                   </Menu.Button>
+                 </div>
+                 <Transition
+                   as={Fragment}
+                   enter="transition ease-out duration-100"
+                   enterFrom="transform opacity-0 scale-95"
+                   enterTo="transform opacity-100 scale-100"
+                   leave="transition ease-in duration-75"
+                   leaveFrom="transform opacity-100 scale-100"
+                   leaveTo="transform opacity-0 scale-95"
+                 >
+                   <Menu.Items className="z-10 origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                     <Menu.Item>
+                       {({ active }) => (
+                         <NavLink
+                           to='/profile'
+                           className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                          >
-                           Sign out
-                         </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
+                           {user?.email} Profile
+                         </NavLink>
+                       )}
+                     </Menu.Item>
+                     <Menu.Item>
+                       {({ active }) => (
+                         <NavLink
+                           to="/"
+                           className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                         >
+                           Settings
+                         </NavLink>
+                       )}
+                     </Menu.Item>
+                     <Menu.Item>
+                       {({ active }) => (
+                         <a
+                         onClick={()=>signOut(auth)}
+                          className={classNames(
+                            active ? "bg-gray-100" : "",
+                            "block px-4 py-2 text-sm text-gray-700"
+                          )}
+                        >
+                          Sign out
+                        </a>
+                       )}
+                     </Menu.Item>
+                   </Menu.Items>
+                 </Transition>
+               </Menu>:
+               <Link  to='/login' >Login</Link>
+               }
               </div>
             </div>
         
