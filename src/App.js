da910aa19a1ffb277833e4bdfea543bc7466352d
srabon0 <srabonemam4@gmail.com>
2022-04-30 11:49:34 +0600

    login signup firebase

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Blog from './components/pages/Blog/Blog';
 import Home from './components/pages/Home/Home/Home';
 import Header from './components/Shared/Header/Header';
 import Footer from './components/Shared/Footer/Footer';
+import Signup from './components/pages/Authentication/Signup/Signup';
+import Login from './components/pages/Authentication/Login/Login';
 function App() {
   return (
     <>
@@ -11,6 +13,8 @@ function App() {
     <Routes>
       <Route path='/' element={<Home></Home>}></Route>
       <Route path='/blog' element={<Blog></Blog>}></Route>
+      <Route path='/signup' element={<Signup></Signup>}></Route>
+      <Route path='/login' element={<Login></Login>}></Route>
     </Routes>
     <Footer></Footer>
     </>
