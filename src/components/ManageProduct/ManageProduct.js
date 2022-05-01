5b9514357d96ac29b8859b1d4e6d03b6d3c2ba3f
srabon0 <srabonemam4@gmail.com>
2022-05-02 01:17:18 +0600

    useItem hook

--- src/components/ManageProduct/ManageProduct.js
+++ src/components/ManageProduct/ManageProduct.js
@@ -1,18 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
+import useItem from "../hooks/useItem";
 
 
 const ManageProduct = () => {
   const { id } = useParams();
-  const [item, setItem] = useState([]);
-  useEffect(()=>{
-    const url = `http://localhost:5000/inventory/${id}`;
-    fetch(url)
-    .then(res=>res.json())
-    .then(data=>{
-      console.log(data);
-      setItem(data)})
-  },[id])
+  const [item,setItem] = useItem(id)
+  
   
 
 
